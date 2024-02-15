25764cccb3b4b471a8ca68cb86fb07803e2bc024
xkrivosik <andrejkrivosik89@gmail.com>
2024-02-15 16:34:08 +0100

    Update haha_funni_game.c

--- haha_funni_game.c
+++ haha_funni_game.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 //mas mali kokot
+//nie ty
 void print_board(char board[3][3]) {
     printf("  1   2   3\n");
     printf("A %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
