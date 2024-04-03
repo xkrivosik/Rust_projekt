ee1c77388356df98974db8ede41ea7fa0cf557a2
TomasStudenc <145460488+TomasStudenc@users.noreply.github.com>
2024-04-03 19:09:33 +0200

    login input

--- fit_rater/src/main.rs
+++ fit_rater/src/main.rs
@@ -1,11 +1,31 @@
-use rocket::fs::NamedFile;
-use std::path::{Path, PathBuf};
-
-#[rocket::get("/<file..>", rank = 2)]
-fn static_files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
-}
+use std::fs::OpenOptions;
+use std::io::{self, Write};
 
 fn main() {
-    ignite().mount("/", routes![routes::index, routes::add_item, routes::get_item, routes::update_item, routes::delete_item, static_files]).launch();
+    let mut username = String::new();
+    let mut password = String::new();
+    loop {
+    println!("Zadaj meno:");
+    username.clear();
+    io::stdin().read_line(&mut username).expect("Failed to read username");
+
+    println!("Zadaj Heslo:");
+    password.clear();
+    io::stdin().read_line(&mut password).expect("Failed to read password");
+   if username.trim().is_empty()||password.trim().is_empty(){
+        println!("invalid input!!!");
+    }
+    else{
+        break;
+    }
+}
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("src/login.txt")
+        .expect("Failed to open file");
+    //comand na print to file 
+    writeln!(file, "{}||{}", username.trim(), password.trim())
+        .expect("Failed to write to file");
+    println!("Username and password saved");
 }
--- fit_rater/target/debug/fit_rater.d
+++ fit_rater/target/debug/fit_rater.d
@@ -1,1 +1,1 @@
-C:\Users\Andy\Desktop\RUST_projekt\fit_rater\target\debug\fit_rater.exe: C:\Users\Andy\Desktop\RUST_projekt\fit_rater\src\main.rs
+C:\Users\titob\OneDrive\Desktop\rust\ projekt\fit_rater\target\debug\fit_rater.exe: C:\Users\titob\OneDrive\Desktop\rust\ projekt\fit_rater\src\main.rs
