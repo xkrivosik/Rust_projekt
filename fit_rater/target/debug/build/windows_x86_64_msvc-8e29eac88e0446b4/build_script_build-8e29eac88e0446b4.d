885b3f2739974f4c1fa3ce0086f5d7f1882371b5
xkrivosik <andrejkrivosik89@gmail.com>
2024-05-09 12:49:25 +0200

    odprezentovane
    bombaklad

--- fit_rater/target/debug/build/windows_x86_64_msvc-8e29eac88e0446b4/build_script_build-8e29eac88e0446b4.d
+++ fit_rater/target/debug/build/windows_x86_64_msvc-8e29eac88e0446b4/build_script_build-8e29eac88e0446b4.d
@@ -1,5 +1,5 @@
-C:\Users\titob\OneDrive\Desktop\rust projekt\fit_rater\target\debug\build\windows_x86_64_msvc-8e29eac88e0446b4\build_script_build-8e29eac88e0446b4.exe: C:\Users\titob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs
+C:\Users\Andy\Desktop\RUST_projekt\fit_rater\target\debug\build\windows_x86_64_msvc-8e29eac88e0446b4\build_script_build-8e29eac88e0446b4.exe: C:\Users\Andy\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs
 
-C:\Users\titob\OneDrive\Desktop\rust projekt\fit_rater\target\debug\build\windows_x86_64_msvc-8e29eac88e0446b4\build_script_build-8e29eac88e0446b4.d: C:\Users\titob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs
+C:\Users\Andy\Desktop\RUST_projekt\fit_rater\target\debug\build\windows_x86_64_msvc-8e29eac88e0446b4\build_script_build-8e29eac88e0446b4.d: C:\Users\Andy\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs
 
-C:\Users\titob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs:
+C:\Users\Andy\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.5\build.rs:
